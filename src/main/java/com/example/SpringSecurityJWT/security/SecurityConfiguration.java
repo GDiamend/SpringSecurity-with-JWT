ad56ca5c340322f03b76bf9ce5fd2541d8a778cb
GDiamend <Gdiamend@gmail.com>
2023-08-02 22:00:55 -0300

    Added security package
    Added security package with basic security's methods.

---
+++ src/main/java/com/example/SpringSecurityJWT/security/SecurityConfiguration.java
@@ -0,0 +1,64 @@
+package com.example.SpringSecurityJWT.security;
+
+import java.beans.Customizer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class SecurityConfiguration {
+	
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		return httpSecurity
+				.csrf(config -> config.disable())
+				.authorizeHttpRequests(auth -> {
+					auth.requestMatchers("/hello").permitAll();
+					auth.anyRequest().authenticated();
+				})
+				.sessionManagement(session ->{
+					session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+				})
+				.httpBasic()
+				.and()
+				.build();
+	}
+	
+	@Bean
+	UserDetailsService userDetailsService() {
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+		manager.createUser(User.withUsername("Santiago")
+				.password("1234")
+				.roles()
+				.build());
+		
+		return manager;
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	@Bean
+	AuthenticationManager authenticationManager(HttpSecurity httpSecurity, PasswordEncoder passwordEncoder) throws Exception {
+		return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class)
+				.userDetailsService(userDetailsService())
+				.passwordEncoder(passwordEncoder)
+				.and().build();		
+	}
+
+}
